ccdac6dfd3d7c329c54a5a708cc0ffdcd1f7975c
= <taylor.johnson@steeldynamics.com>
2026-01-21 07:56:00 -0500

    wip lexicographical sequence

---
+++ twopointers/nextLexicographicalSeq.py
@@ -0,0 +1,20 @@
+from typing import List
+
+def main():
+    """
+        Rearranges the string's characters into a new string represent the next immediate
+        lexicographical sequence.
+
+        Lexicographical order is alphabetical order in a numerical sense.
+
+        If "abc" is the first lexicographical sequence, "acb" is the next immediate sequence,
+        because if "abc" equals 123 the next possible sequence is 132. 
+
+        If "cba" is the last lexicographical sequence, "abc" is the next immediate sequence,
+        because it's the first possible sequence.
+    """
+
+
+
+def test_main():
+
