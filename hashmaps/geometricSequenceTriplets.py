e933370f3b7fcc595be69788ea8413141063e150
Taylor <johnsoct@hey.com>
2026-02-01 16:20:12 -0500

    followed the guide

--- hashmaps/geometricSequenceTriplets.py
+++ hashmaps/geometricSequenceTriplets.py
@@ -1,35 +1,38 @@
+from collections import defaultdict
 from typing import List
 
 def find_geometric_sequence_triplets(
     nums: List[int],
     ratio: int
 ) -> int:
-    nums_hashset = set(nums)
-    triplets = []
+    """
+        Finds a sequence of geometric triplets in a list of integers.
 
+        A sequence of geometric triplets is equivalent of 3 numbers in a row which:
+        1. Values are equivalent to x, xr, xr^2
+        2. Indices of each value in the list are x < xr < xr^2
+    """
+
+    left_map = defaultdict(int)
+    right_map = defaultdict(int)
+    count = 0
+
+    # Store the frequency of each number in our right map
     for num in nums:
-        # calculate x, x(r), and x(r^2)
-        x = num
-        xr = num * ratio
-        xr2 = num * (ratio**2)
+        right_map[num] += 1
 
-        print(x, xr, xr2)
+    # Search for triplets (num represents the middle value)
+    for num in nums:
+        # Decrease the frequency of num in the right_map since num represents the new current middle
+        right_map[num] -= 1
 
-        # Check if x(r) and x(r^2) are in nums_hashset
-        in_hashset = xr in nums_hashset and xr2 in nums_hashset
-        if not in_hashset:
-            continue
+        if num % ratio == 0:
+           count += left_map[num // ratio] * right_map[num * ratio]
 
-        # Check indexes are found to be x < xr < xr2
-        valid_indices = nums.index(x) < nums.index(xr) < nums.index(xr2)
-        if not valid_indices:
-            continue
+        # Increase the frequency of num in the left_map since it will no longer be the current middle
+        left_map[num] += 1
 
-        # Add to triplets
-        triplets.append([ x, xr, xr2 ])
-
-    print(triplets)
-    return len(triplets)
+    return count
 
 def test_find_geometric_sequence_triplets():
     assert find_geometric_sequence_triplets([2, 1, 2, 4, 8, 8], 2) == 5
